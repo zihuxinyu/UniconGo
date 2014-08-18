836dacd79abc82cc7016cb11aed28fdccbed39e9
weibaohui <zihuxinyu@163.com>
2014-08-18 22:49:57 +0800

    完善基础功能

---
+++ src/UnionGo/Library/Time.go
@@ -0,0 +1,11 @@
+package Library
+
+import (
+	"time"
+)
+const TIME_LAYOUT_OFTEN = "2006-01-02 15:04:05"
+// 解析常用的日期时间格式：2014-01-11 16:18:00，东八区
+func TimeParseOften(value string) (time.Time, error) {
+	local, _ := time.LoadLocation("Local")
+	return time.ParseInLocation(TIME_LAYOUT_OFTEN, value, local)
+}
