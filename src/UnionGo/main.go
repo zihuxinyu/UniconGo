132b54c007836b87f9399370f900cb20452b6ac7
weibaohui <zihuxinyu@163.com>
2014-08-22 17:15:24 +0800

    完成基本批量操作功能

--- src/UnionGo/main.go
+++ src/UnionGo/main.go
@@ -5,34 +5,35 @@ import (
 	"github.com/astaxie/beego"
 	"UnionGo/controllers"
 	"github.com/astaxie/beego/orm"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql" // import your used driver
+	"reflect"
+	"fmt"
 )
-
-var Cfg = beego.AppConfig
 
 func init() {
 
 
-	dbUser := Cfg.String("db_user")
-	dbPass := Cfg.String("db_pass")
-	dbHost := Cfg.String("db_host")
-	dbPort := Cfg.String("db_port")
-	dbName := Cfg.String("db_name")
-	maxIdleConn, _ := Cfg.Int("db_max_idle_conn")
-	maxOpenConn, _ := Cfg.Int("db_max_open_conn")
-	dbLink := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", dbUser, dbPass, dbHost, dbPort, dbName) + "&loc=Asia%2FChongqing"
-	fmt.Println(dbLink)
+	dbUser, _ := beego.GetConfig("string", "db_user")
+	dbPass, _ := beego.GetConfig("string", "db_pass")
+	dbHost, _ := beego.GetConfig("string", "db_host")
+	dbPort, _ := beego.GetConfig("string", "db_port")
+	dbName, _ := beego.GetConfig("string", "db_name")
+//	maxIdleConn, _ := beego.AppConfig.Int("db_max_idle_conn")
+//	maxOpenConn, _ :=  beego.AppConfig.Int( "db_max_open_conn")
+	maxIdleConn, _ := beego.GetConfig("int","db_max_idle_conn")
+	maxOpenConn, _ := beego.GetConfig("int", "db_max_open_conn")
+	beego.Debug("dd",reflect.TypeOf(maxIdleConn),reflect.TypeOf(maxOpenConn))
+	dbLink := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", dbUser, dbPass, dbHost, dbPort, dbName)
 	orm.RegisterDriver("mysql", orm.DR_MySQL)
-	orm.RegisterDataBase("default", "mysql", dbLink, maxIdleConn, maxOpenConn)
+	orm.RegisterDataBase("default", "mysql", dbLink, maxIdleConn.(int), maxOpenConn.(int))
 
 
 }
 
 func main() {
-	fmt.Println("%#v",orm.NewOrm())
+
 	orm.Debug = true
-	beego.SessionOn=true
+	beego.SessionOn = true
 	beego.AutoRouter(&controllers.MainController{})
 	beego.Run()
 
